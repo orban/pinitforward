eb3572628006634acbac54d112d26f2a7c487664
Jonathan Dinu <jondinu@gmail.com>
2021-11-23 16:05:18 -0500

    fix fonts

--- src/App.vue
+++ src/App.vue
@@ -105,7 +105,7 @@
 </template>
 
 <style>
-@import url("https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100;0,300;0,400;0,700;1,300;1,400;1,700&family=Source+Code+Pro:ital,wght@0,400;0,700;1,400;1,700&family=Source+Sans+Pro:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100;0,300;0,400;0,700;1,300;1,400;1,700&family=Source+Code+Pro:ital,wght@0,400;0,700;1,400;1,700&family=Source+Sans+Pro:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap");
 
 *,
 *:before,
